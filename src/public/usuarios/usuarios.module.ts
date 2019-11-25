097263ad939c7b13e9eac58305d2371e279bc147
andres lopez <calopez@asertisas.com>
2019-11-25 00:04:36 -0500

    set relationship usuarios<->profesionales

--- src/public/usuarios/usuarios.module.ts
+++ src/public/usuarios/usuarios.module.ts
@@ -1,11 +1,12 @@
-import { CommonFunctionsController } from '../../common/common-functions.controller';
-import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
-import { UsuariosMiddleware } from './usuarios.middleware';
-import { CommonModule } from '../../common/common.module';
+import { CommonFunctionsController } from '../../common/common-functions.controller'
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common'
+import { UsuariosMiddleware } from './usuarios.middleware'
+import { CommonModule } from '../../common/common.module'
+import { UsuariosController } from './usuarios.controller'
 
 @Module({
   imports: [CommonModule],
-  controllers: [CommonFunctionsController],
+  controllers: [UsuariosController, CommonFunctionsController],
 })
 export class UsuariosModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
